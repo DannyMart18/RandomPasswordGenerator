fb55b3631711ce4df51d01829af260c5da56d3e4
DannyMart18 <58661521+DannyMart18@users.noreply.github.com>
2021-07-21 10:34:13 +0100

    Update PasswordGenerator.java

--- PasswordGenerator/src/PasswordGenerator.java
+++ PasswordGenerator/src/PasswordGenerator.java
@@ -38,17 +38,36 @@ public class PasswordGenerator {
         }
 
     }
+    
+    //Create a function that randomly generates a letter (lowercase or uppercase) or number (0-9) using ASCII
+	//'0' - '9' => 48-57 in ASCII
+	//'A' - 'Z' => 65-90 in ASCII
+	//'a' - 'z' => 97-122 in ASCII
 
     public static char randomCharacter(){
         int rand = (int)(Math.random()* 62);
 
+        
+        	//0-61 inclusive = all possible values of rand
+		//0-9 inclusive = 10 possible numbers/digits
+		//10-35 inclusive = 26 possible uppercase letters
+		//36-61 inclusive = 26 possible lowercase letters
+		//If rand is between 0 (inclusive) and 9 (inclusive), we can say it's a number/digit
+		//If rand is between 10 (inclusive) and 35 (inclusive), we can say it's an uppercase letter
+		//If rand is between 36 (inclusive) and 61 (inclusive), we can say it's a lowercase letter
         if(rand < 9){
+            //Number (48-57 in ASCII)
+			//To convert from 0-9 to 48-57, we can add 48 to rand because 48-0 = 48
             int number = rand + 48;
             return (char)(number);
         }else if(rand <= 35){
+            //Uppercase letter (65-90 in ASCII)
+			//To convert from 10-35 to 65-90, we can add 55 to rand because 65-10 = 55
             int uppercase = rand + 55;
             return (char)(uppercase);
         }else{
+            //Lowercase letter (97-122 in ASCII)
+			//To convert from 36-61 to 97-122, we can add 61 to rand because 97-36 = 61
             int lowercase = rand + 61;
             return (char)(lowercase);
         }
